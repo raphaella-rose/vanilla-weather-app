64f19a4c98fc2d4b9cd3bc8876df7b2e183e5ef4
raphaella-rose <raphaella.rose1@gmail.com>
2021-07-13 15:46:15 +0100

    added relevant icon from api & updated max & min temperature

--- vanilla-weather-app/src/app.js
+++ vanilla-weather-app/src/app.js
@@ -14,23 +14,31 @@ return `${day} ${hours}:${minutes}`;
 }
 
 function displayTemperature(response) {
-let temperatureElement = document.querySelector("#temperature")
-let cityElement = document.querySelector("#city")
-let descriptionElement = document.querySelector("#description")
-let humidityElement = document.querySelector("#humidity")
-let windspeedElement = document.querySelector("#windspeed")
-let dateElement = document.querySelector("#date")
+let temperatureElement = document.querySelector("#temperature");
+let cityElement = document.querySelector("#city");
+let descriptionElement = document.querySelector("#description");
+let humidityElement = document.querySelector("#humidity");
+let windspeedElement = document.querySelector("#windspeed");
+let dateElement = document.querySelector("#date");
+let iconElement = document.querySelector("#icon");
+let maxMinElement = document.querySelector("#max-min-temp");
+let maxTemperature = Math.round(response.data.main.temp_max);
+let minTemperature = Math.round(response.data.main.temp_min);
 temperatureElement.innerHTML = Math.round(response.data.main.temp);
 cityElement.innerHTML = response.data.name;
 descriptionElement.innerHTML = response.data.weather[0].description;
 humidityElement.innerHTML = `humidity: ${response.data.main.humidity}%`;
 windspeedElement.innerHTML = `windspeed: ${Math.round(response.data.wind.speed)} Km/H`;
+maxMinElement.innerHTML = `High: ${maxTemperature}° Low: ${minTemperature}°`
 dateElement.innerHTML = formatDate(response.data.dt * 1000);
+iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 
 let apiKey = "e99a6b83a02b9bd00c5d8a973f6c63e0";
 let unit = "metric"
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=${unit}`
+let city = "London"
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`
  
 axios.get(apiUrl).then(displayTemperature)
