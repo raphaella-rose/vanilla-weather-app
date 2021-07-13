c539fd7d78f2dcc70bfef8659b08f718b0e9779f
raphaella-rose <raphaella.rose1@gmail.com>
2021-07-13 16:05:53 +0100

    added the search engine

--- vanilla-weather-app/src/app.js
+++ vanilla-weather-app/src/app.js
@@ -34,11 +34,19 @@ dateElement.innerHTML = formatDate(response.data.dt * 1000);
 iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 iconElement.setAttribute("alt", response.data.weather[0].description);
 }
-
-
+function search(city) {
 let apiKey = "e99a6b83a02b9bd00c5d8a973f6c63e0";
 let unit = "metric"
-let city = "London"
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`
- 
 axios.get(apiUrl).then(displayTemperature)
+} 
+
+function handleSubmit(event) {
+event.preventDefault();
+let cityInputElement = document.querySelector("#location-input");
+search(cityInputElement.value);
+}
+
+
+let form = document.querySelector("#search-bar");
+form.addEventListener("submit", handleSubmit);
